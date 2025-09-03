ebbec5f447dff073cf2375f31b1691f8885319fe
PedroOttavio <pedroottavioss@gmail.com>
2025-09-03 11:41:51 -0300

    frontend quase finalizado, falta corrigir a largura da linha

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,21 @@
 function funcao_clique() {
     window.alert('Oi!');
 }
+
+// Código para visualizar e ocultar senha 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('togglePassword').addEventListener('click', function() {
+        const passwordField = document.getElementById('password');
+        const eyeIcon = document.getElementById('eyeIcon');
+        
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            eyeIcon.classList.remove('fa-eye');
+            eyeIcon.classList.add('fa-eye-slash');
+        } else {
+            passwordField.type = 'password';
+            eyeIcon.classList.remove('fa-eye-slash');
+            eyeIcon.classList.add('fa-eye');
+        }
+    });
+});
