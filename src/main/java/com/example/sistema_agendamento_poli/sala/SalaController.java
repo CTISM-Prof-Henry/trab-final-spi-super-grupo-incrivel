92517299c65e127662ff68dc431e1dd3e3273f13
PedroOttavio <pedroottavioss@gmail.com>
2025-11-11 20:38:14 -0300

    Revert "editei o SalaController para chamar a página sala"
    This reverts commit 52c52fdcb4d342e8c5cdbd005403e12d22aad9a9.

--- src/main/java/com/example/sistema_agendamento_poli/sala/SalaController.java
+++ src/main/java/com/example/sistema_agendamento_poli/sala/SalaController.java
@@ -1,12 +1,6 @@
 package com.example.sistema_agendamento_poli.sala;
 
 // Imports do Spring Web otimizados
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/salas")
 public class SalaController {
@@ -23,14 +20,6 @@ public class SalaController {
     private SalaService salaService;
     public SalaController(SalaService salaService) {
         this.salaService = salaService;}
-
-        // espero que funcione kkkkkk
-    // --- Novo endpoint: redireciona para a página estática sala.html ---
-    @GetMapping("/pagina")
-    public ResponseEntity<Void> abrirPaginaSala() {
-        URI uri = URI.create("/Front-end/telaSala/sala.html");
-        return ResponseEntity.status(HttpStatus.FOUND).location(uri).build();
-    }
 
     // --- MÉTODOS DE CONSULTA (Retornam Entidade) ---
 
